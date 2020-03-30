715efb44fcd4f8b0582115c26a20ef5a4bf784a9
tomron27 <tomron27@gmail.com>
2020-03-30 13:39:03 +0300

    Added dicom and nifti loading methods for CT-82 dataset

--- dataio/loader/utils.py
+++ dataio/loader/utils.py
@@ -2,9 +2,40 @@ import nibabel as nib
 import numpy as np
 import os
 from utils.util import mkdir
+import SimpleITK as sitk
+
 
 def is_image_file(filename):
-    return any(filename.endswith(extension) for extension in [".nii.gz"])
+    return any(filename.endswith(extension) for extension in [".nii.gz", ".dcm"])
+
+
+def load_dicom_dir(dicom_path):
+    """
+    Load a folder with 3D dicom sequence (for CT-82 Dataset)
+    :param dicom_path: str, path to dicom folder
+    :return: ndarray, 3D array representation of volumetric data
+    """
+    reader = sitk.ImageSeriesReader()
+    dicom_names = reader.GetGDCMSeriesFileNames(dicom_path)
+    reader.SetFileNames(dicom_names)
+    img = reader.Execute()
+    img = sitk.GetArrayFromImage(img)[::-1]
+    img = np.flip(img, axis=0)
+    img = np.rot90(img, k=2, axes=(1,2))
+    return img
+
+
+def load_nifti_mask(mask_path):
+    """
+    Load NifTi 3D mask data, to work in conjunction with load_dicom_dir (for CT-82 Dataset)
+    :param mask_path: path to "labelXXXX.nii.gz" mask file
+    :return: ndarray, 3D array representation of volumetric mask data
+    """
+    mask = nib.load(mask_path)
+    mask = np.transpose(mask.get_fdata(), (2, 0, 1))
+    mask = np.flip(mask, axis=1)
+    mask = np.rot90(mask, k=1, axes=(1,2))
+    return mask
 
 
 def load_nifti_img(filepath, dtype):
